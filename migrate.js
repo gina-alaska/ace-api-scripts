c11dd3631e24f61d1f4e51e80087c05458a72090
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-07-27 11:35:34 -0800

    Use new LoopBack weather reports endpoint that includes position.

--- migrate.js
+++ migrate.js
@@ -19,11 +19,17 @@ loopbackAgent.initialize('/MobileUsers/login', {
     // local LoopBack API supports the GET operation on the
     // /WeatherReports endpoint via the "Explorer" interface at:
     // http://localhost:3000/explorer/
-    return loopbackAgent.get('/WeatherReports');
+    return loopbackAgent.get('/WeatherReports/with-positions');
   })
   .then(function (results) {
     // The asynchronous loopbackAgent.get() task has finished.
     //
     // Output the results of the GET operation.
-    console.log(results);
+    console.log('Timestamp: ' + results.WeatherReports[0].Position.timestamp);
+    console.log('Latitude: ' + results.WeatherReports[0].Position.latlng.lat);
+    console.log('Longitude: ' + results.WeatherReports[0].Position.latlng.lng);
+    console.log('Cloud Cover: ' + results.WeatherReports[0].cloudCover);
+    console.log('Precipitation: ' + results.WeatherReports[0].precipitation);
+    console.log('Visibility: ' + results.WeatherReports[0].visibility);
+    // Etc.
   });
